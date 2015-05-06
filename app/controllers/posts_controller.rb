ea9e582ac4a98b08a5a31391b1fc6390743ad4c1
Jack Hou <jack.hou@live.com>
2015-05-06 15:48:30 +1200

    Add voting

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 
-	before_action :find_posts, only: [:show, :update, :edit, :destroy]
+	before_action :find_posts, only: [:show, :update, :edit, :destroy, :upvote, :downvote]
 	before_action :authenticate_user!, except: [:index, :show]
 
 	def index
@@ -39,6 +39,17 @@ class PostsController < ApplicationController
 	def destroy
 		@post.destroy
 		redirect_to root_path
+	end
+
+	# the following upvote and downvote are utilized in the routes.rb file
+	def upvote
+		@post.upvote_by current_user
+		redirect_to :back
+	end
+
+	def downvote
+		@post.downvote_by current_user
+		redirect_to :back
 	end
 
 	private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
     resources :posts do
+    	# the upvote and downvote method are defined in the controller
+    	member do
+    		get 'like', to: 'posts#upvote'
+    		get 'dislike', to: 'posts#downvote'
+    	end
     	resources :comments
     end
 
